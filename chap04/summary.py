22d3747de3d3e76ca995ef8696ffd53e26ffbd19
Kimhannah <skqkdldhf98@naver.com>
2025-10-04 11:16:57 +0900

    텍스트 요약 기능

--- chap04/summary.py
+++ chap04/summary.py
@@ -45,11 +45,11 @@ def summarize_txt(file_path: str): # ①
     return response.choices[0].message.content
 
 if __name__ == '__main__':
-    file_path = './chap04/output/과정기반 작물모형을 이용한 웹 기반 밀 재배관리 의사결정 지원시스템 설계 및 구축_with_preprocessing.txt'
+    file_path = './output/과정기반 작물모형을 이용한 웹 기반 밀 재배관리 의사결정 지원시스템 설계 및 구축_with_preprocessing.txt'
 
     summary = summarize_txt(file_path)
     print(summary)
 
     # ⑤ 요약된 내용을 파일로 저장한다.
-    with open('./chap04/output/crop_model_summary.txt', 'w', encoding='utf-8') as f:
+    with open('./output/crop_model_summary.txt', 'w', encoding='utf-8') as f:
         f.write(summary)
